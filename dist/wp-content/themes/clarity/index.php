f73c5c6eeb1a96db4f9982b12436a749bef39e91
Alex Ward <alex@volcaniccreations.com>
2016-01-14 22:33:17 +0000

    started work on jetpack styling

--- dist/wp-content/themes/clarity/index.php
+++ dist/wp-content/themes/clarity/index.php
@@ -14,7 +14,7 @@ while (have_posts()) : the_post() ?>
 				<div class="c-post-listing__entry-content">
                     <header>
                         <h1><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h1>
-                        <span class="c-post-listing__entry-date"><abbr class="published" title="<?php the_time('Y-m-d') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_time('jS F, Y') ) ?></abbr></span>
+                        <span class="c-post-listing__entry-date"><abbr class="published" title="<?php the_time('Y-m-d') ?>"><?php unset($previousday); the_time('jS F, Y') ?></abbr></span>
                         <span class="c-post-listing__category"><?php the_category() ?></span>
                         <span class="c-post-listing__comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
                     </header>
